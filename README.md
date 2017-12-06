1ea19375251d6d8f014782ff1bed14e0651ccc5a
Felipe F M <felipefm.suporte@gmail.com>
2017-12-06 07:38:09 -0200

    versão 2.9
    Retirada a opção de hora do bot. Agora o bot pode rodar em qualquer horario sem restrições.

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Unica coisa que peço é que informe a fonte. Se possível, me chame no Telegram e me comunique também.
 @FelipeFM
 
+Fale comigo: [![Join the bot support group on Telegram](https://img.shields.io/badge/Suport%20Telegram-%40FelipeFM-blue.svg)](https://telegram.me/FelipeFM)
+Compartilhe: [![Twitter](https://img.shields.io/twitter/url/https/github.com/felipefm/bot-Telegram-Imgur.svg?style=social)](https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fgithub.com%2Ffelipefm%2Fbot-Telegram-Imgur)
+Doações com PagueSeguro - [![Doações](https://img.shields.io/badge/Fa%C3%A7a%20sua%20Contribui%C3%A7%C3%A3o-PagueSeguro-yellow.svg)](https://pag.ae/blrNM4g)
+
+
 # O que o BOT faz?
 Ele visualiza as 3 primeiras imagens que se destacaram como "viral" do site Imgur.com e envia para o canal que eu criei.
 Antes de enviar para o canal, o bot verifica se a imagem em questão já foi enviada anteriormente ou não. Caso tenha enviado a imagem, o BOT não faz nada e vai para a próxima imagem.
@@ -13,7 +18,7 @@ $ python3 imgur.py
 ```
 
 ### Versões:
-O BOT ao longo do seu desenvolvimento, já passou por vários outras versões. Hoje está na versão 2.8
+O BOT ao longo do seu desenvolvimento, já passou por vários outras versões. Hoje está na versão 2.9
 
 ### Funcionalidades futuras:
 * Colocar um agendamento no crontab, para que o BOT rode sem nenhum comando do usuário.
--- imgur.py
+++ imgur.py
@@ -9,9 +9,10 @@ import os #DELETAR ARQUIVOS
 from io import open as iopen
 from urllib.parse import urlsplit
 
-# VERSÃO 2.1
+# VERSÃO 2.9
 # BOT AINDA EM DESENVOLVIMENTO
-# ATUALIZADO EM 2017-12-04
+# ATUALIZADO EM 2017-12-06
+# RETIREI A PARTE DA HORA. ASSIM O BOT PODE RODAR EM QUALQUER HORARIO.
 
 up = Updater('')
 
@@ -29,56 +30,51 @@ def requests_image(file_url):
 
 
 def go(bot, update):
-    # import time
-    hora = strftime('%H')
-    pprint (int(hora))
-    if(int(hora) <= 23 or int(hora) > 8):
-        # pdb.set_trace() #CODIGO SERA DEBUGADO APARTIR DAQUI
-        page = requests.get("https://imgur.com/gallery/hot/viral/page/1/hit?scrolled&set=0")
-        soup = BeautifulSoup(page.content, 'html.parser')
-        i=1
-        for a in soup.find_all('a', href=True):
-            if i <= 3:
-                pprint (i)
-                url = a['href']
-                msg = "http://imgur.com"+url
-                msg2 = msg
-                page2 = requests.get(msg2)
-                soup2 = BeautifulSoup(page2.content, 'html.parser')
-                url_img = "http:"+soup2.find_all('img')[0]['src']
-                exten = url_img.split('.')
-                suffix = exten[3]
-                nome_arquivo = url_img.split('/')
-                nome_file = nome_arquivo[3]
-                if(suffix == "jpg" or suffix == "png"):
-                    descricao_img = soup2.find_all('p')[0].get_text()
-                    if descricao_img == "Use old embed code":
-                        descricao_img = " " + url_img
-                    # print (descri_img)
-                    titulo_img = soup2.select('h1.post-title')[0].text.strip()
-                    descri_img = titulo_img + "\n" +descricao_img
-                    conn = sqlite3.connect("imgur.db")
-                    cursor = conn.cursor()
-                    sql = "SELECT * FROM imgur WHERE url=?"
-                    cursor.execute(sql, [(msg2)])
-                    user1 = cursor.fetchone() #retrieve the first row
-                    if(user1 == None):
-                        requests_image(url_img)
-                        cursor.execute("INSERT INTO imgur (url, url_img, descri_img, nome_file) VALUES (?,?,?,?)", (msg2,url_img,descri_img,nome_file,))
-                        conn.commit()
-                        conn.close()
-                        msg += "\n" + descri_img
-                        # msg += "\n Este Bot Ainda esta em fase de testes. Qualquer problema, por favor, informe @FelipeFM."
-                        channel = "@ChannelBotImgur"
-                        # bot.send_message(chat_id=channel, text=descri_img, photo=open(nome_file, 'rb'))
-                        bot.send_photo(chat_id=channel, photo=open(nome_file, 'rb'), caption=descri_img) #SÓ ENVIA 200 CARACTERES NA DESCRIÇÃO DA FOTO
-                        print(nome_file+" Imagem enviada para o canal.")
-                        os.remove(nome_file) #REMOVO A FOTO QUE SALVEI
-                        print(nome_file + " Imagem removida do HD.")
-                    else:
-                        print (nome_file + " Imagem ja existe no banco de dados.")
-                i+=1
-                # os.remove(nome_file) #REMOVO A FOTO QUE SALVEI
+    # pdb.set_trace() #CODIGO SERA DEBUGADO APARTIR DAQUI
+    page = requests.get("https://imgur.com/gallery/hot/viral/page/1/hit?scrolled&set=0")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    i=1
+    for a in soup.find_all('a', href=True):
+        if i <= 3:
+            pprint (i)
+            url = a['href']
+            msg = "http://imgur.com"+url
+            msg2 = msg
+            page2 = requests.get(msg2)
+            soup2 = BeautifulSoup(page2.content, 'html.parser')
+            url_img = "http:"+soup2.find_all('img')[0]['src']
+            exten = url_img.split('.')
+            suffix = exten[3]
+            nome_arquivo = url_img.split('/')
+            nome_file = nome_arquivo[3]
+            if(suffix == "jpg" or suffix == "png"):
+                descricao_img = soup2.find_all('p')[0].get_text()
+                if descricao_img == "Use old embed code":
+                    descricao_img = " " + url_img
+                # print (descri_img)
+                titulo_img = soup2.select('h1.post-title')[0].text.strip()
+                descri_img = titulo_img + "\n" +descricao_img
+                conn = sqlite3.connect("imgur.db")
+                cursor = conn.cursor()
+                sql = "SELECT * FROM imgur WHERE url=?"
+                cursor.execute(sql, [(msg2)])
+                user1 = cursor.fetchone() #retrieve the first row
+                if(user1 == None):
+                    requests_image(url_img)
+                    cursor.execute("INSERT INTO imgur (url, url_img, descri_img, nome_file) VALUES (?,?,?,?)", (msg2,url_img,descri_img,nome_file,))
+                    conn.commit()
+                    conn.close()
+                    msg += "\n" + descri_img
+                    # msg += "\n Este Bot Ainda esta em fase de testes. Qualquer problema, por favor, informe @FelipeFM."
+                    channel = "@ChannelBotImgur"
+                    # bot.send_message(chat_id=channel, text=descri_img, photo=open(nome_file, 'rb'))
+                    bot.send_photo(chat_id=channel, photo=open(nome_file, 'rb'), caption=descri_img) #SÓ ENVIA 200 CARACTERES NA DESCRIÇÃO DA FOTO
+                    print(nome_file+" Imagem enviada para o canal.")
+                    os.remove(nome_file) #REMOVO A FOTO QUE SALVEI
+                    print(nome_file + " Imagem removida do HD.")
+                else:
+                    print (nome_file + " Imagem ja existe no banco de dados.")
+            i+=1
 
 up.dispatcher.add_handler(CommandHandler('go', go))
 
